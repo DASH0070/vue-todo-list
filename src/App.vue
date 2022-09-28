31d0a572e75a5295fd2fe12a351ffcc801796a2e
DASH <shivansh.tyagi@vidyamantra.com>
2022-09-28 16:59:12 +0530

    Created AddItem

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,28 @@
 <script setup lang="ts">
-	import { ref } from 'vue'
-	const inputText = ref('')
-	const pr = () => {
-		console.log(inputText.value)
-		inputText.value = ''
-	}
+import { ref } from 'vue'
+let id = 3
+const inputText = ref('')
+const todos = ref([
+	{ id: 0, text: 'Apple' },
+	{ id: 1, text: 'Mango' },
+	{ id: 2, text: 'Orange' }
+])
+const addItem = () => {
+	const obj = { id: id++, text: inputText.value}
+	todos.value.push(obj);
+	inputText.value = ''
+}
+
 </script>
 
 <template>
-  <form @submit.prevent="pr">
-    <input type="text" v-model='inputText' />
-	<button type="submit">Add Todo</button>
-  </form>
+	<form @submit.prevent="addItem">
+		<input type="text" v-model='inputText' />
+		<button type="submit">Add Todo</button>
+	</form>
+	<ul>
+		<li v-for="todo in todos" :key="todo.id">{{todo.text}}</li>
+	</ul>
 </template>
 
 <style scoped>
