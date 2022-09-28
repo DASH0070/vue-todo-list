c862b80a39ab596af0677f16ab33a09f14593b61
DASH <shivansh.tyagi@vidyamantra.com>
2022-09-28 16:37:44 +0530

    Created Project

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+	import { ref } from 'vue'
+	const inputText = ref('')
+	const pr = () => {
+		console.log(inputText.value)
+		inputText.value = ''
+	}
+</script>
+
+<template>
+  <form @submit.prevent="pr">
+    <input type="text" v-model='inputText' />
+	<button type="submit">Add Todo</button>
+  </form>
+</template>
+
+<style scoped>
+
+</style>
