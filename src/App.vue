9c6a74b39e5eec21f877cd5d04bfa768593b2e15
DASH <shivansh.tyagi@vidyamantra.com>
2022-09-28 17:31:55 +0530

    final commit

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,22 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+
 let id = 3
 const inputText = ref('')
+
 const todos = ref([
 	{ id: 0, text: 'Apple' },
 	{ id: 1, text: 'Mango' },
 	{ id: 2, text: 'Orange' }
 ])
 const addItem = () => {
-	const obj = { id: id++, text: inputText.value}
+	const obj = { id: id++, text: inputText.value }
 	todos.value.push(obj);
 	inputText.value = ''
+}
+
+const removeItem = (todo: object) => {
+	todos.value = todos.value.filter(elem => elem !== todo)
 }
 
 </script>
@@ -21,7 +27,10 @@ const addItem = () => {
 		<button type="submit">Add Todo</button>
 	</form>
 	<ul>
-		<li v-for="todo in todos" :key="todo.id">{{todo.text}}</li>
+		<li v-for="todo in todos" :key="todo.id" class="flex" >
+			<p>{{todo.text}}</p>
+			<button @click="removeItem(todo)" class="w-40">X</button>
+		</li>
 	</ul>
 </template>
 
