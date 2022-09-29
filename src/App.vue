15dbb6f20badadb3747ae0e5d7895ee36de8f408
DASH <shivansh.tyagi@vidyamantra.com>
2022-09-29 10:40:17 +0530

    ADDED REMOVE ALL

--- src/App.vue
+++ src/App.vue
@@ -26,12 +26,12 @@ const renameItem = (event: MouseEvent, todo: { id: number, text: string }) => {
 		btn.innerText = '+'
 		inp?.removeAttribute('readonly')
 		inp?.focus();
-		inp?.classList.add('border-2', 'rounded-lg')
+		inp?.classList.remove('border-transparent')
 	}
 	else if (btn?.innerText == '+') {
 		btn.innerText = '<>'
 		inp?.setAttribute('readonly', 'readonly')
-		inp?.classList.remove('border-2', 'rounded-lg')
+		inp?.classList.add('border-transparent')
 	}
 }
 
@@ -39,18 +39,24 @@ const renameItem = (event: MouseEvent, todo: { id: number, text: string }) => {
 
 <template>
 	<form @submit.prevent="addItem" class="my-6">
-		<input type="text" v-model='inputText' class="border-2 rounded-lg mx-6" id="add-item-input" />
+		<input type="text" v-model='inputText' class="border-2 rounded-lg mx-6" id="add-item-input"
+			autocomplete="off" />
 		<button type="submit"
-			class="bg-slate-500 rounded-full w-28 h-8 text-slate-50 hover:text-slate-500 hover:bg-slate-50">Add
-			Todo</button>
+			class="bg-slate-500 rounded-full w-28 h-8 text-slate-50 hover:text-slate-500 hover:bg-slate-200 duration-200 border-2 border-slate-500">
+			Add Todo
+		</button>
 	</form>
 	<ul class="flex flex-col justify-center items-center">
 		<li v-for="todo in todos" :key="todo.id" class="flex my-1">
-			<input :value="todo.text" readonly class="outline-none">
+			<input :value="todo.text" readonly class="outline-none border-transparent border-2 rounded-lg">
 			<button @click="removeItem(todo)" class="mx-6 text-red-700">X</button>
 			<button class="text-green-600" @click="(event) => renameItem(event, todo)">{{'<>'}}</button>
 		</li>
 	</ul>
+	<button @click="() => {todos = []}"
+		class="bg-red-700 text-slate-50 w-32 h-8 rounded-full hover:text-red-700 border-2 border-red-700 hover:bg-slate-200 duration-200">
+		Remove All
+	</button>
 </template>
 
 <style scoped>
